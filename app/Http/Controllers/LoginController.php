ee65dfe21238183c3e11343f356ab56b8f4069fa
jhreyes483 <jhreyes483@misena.edu.co>
2023-12-14 22:42:24 -0500

    relaciones en modelos y llamarlas en consultas

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Admin;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Ficha;
+use App\Models\Jornada;
+use App\Models\Instructor;
+use Illuminate\Support\Facades\DB;
+
+class LoginController extends Controller
+{
+    public function form_login()
+    {
+        return view('modules.auth.form_login');
+    }
+    public function auth(Request $request)
+    {
+        $user = Admin::where('email', '=', $request->input('email'))->first();
+        if ($user) {
+            if ($user->password == $request->input('password')) {
+                session(['USER' => $user]);
+                return redirect()->route('dashboard.index');
+            } else {
+                return redirect()->route('login.form_login');
+            }
+        } else {
+            return redirect()->route('login.form_login');
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->flush();
+
+        return redirect()->route('login.form_login');
+    }
+
+
+
+    public function test()
+    {
+
+        /*
+      SELECT 
+      j.nom_jornada,
+      f.No_ficha,
+      a.nom1 aprendiz_name,
+      j.id jornada_id
+      FROM jornada j
+      INNER JOIN ficha f ON f.FK_jornada = j.id
+      INNER JOIN aprendiz a ON a.FK_ficha = f.id_ficha
+
+      INNER JOIN instructor_ficha imf ON imf.FK_ficha = f.id_ficha
+      INNER JOIN instructor i ON i.id_instructor = FK_instructor
+      where j.id =1
+;
+        */
+
+
+        // $instrunctores = Ficha::with('instructores')->get();
+        $jornadaId = 1;
+        // $resultados = Jornada::with('fichas.aprendices')->find($jornadaId);
+        //DB::enableQueryLog();
+
+        /*************************************** */
+        /*
+        select 
+        * from 
+        ficha f
+        INNER JOIN jornada j ON f.FK_jornada = j.id 
+        where j.id = 1
+        */
+
+        $result = Jornada::with('fichas')->find($jornadaId);
+        dump(['q1' => $result->toArray()]);
+        /****************************************** */
+        /*
+        SELECT 
+        *
+        FROM jornada j
+        INNER JOIN ficha f ON f.FK_jornada = j.id
+        INNER JOIN aprendiz a ON a.FK_ficha = f.id_ficha
+        where j.id =1
+        */
+
+        $result = Ficha::with('aprendices', 'jornada')
+            ->whereIn('id_ficha', [1, 3])
+            ->get();
+        dump(['q2' => $result->toArray()]);
+
+        /** uso de whereHas */
+        $result  = Ficha::with('aprendices', 'jornada')
+            ->whereHas('jornada', function ($query) {
+                $query->where('id', 1);
+            })
+            ->get();
+        dump(['q3' => $result->toArray()]);
+
+
+        /** uso de whereHas */
+        $result  = Ficha::with('aprendices', 'jornada', 'instructores')
+            ->whereHas('jornada', function ($query) {
+                $query->where('id', 1);
+            })
+            ->get();
+        dump(['q4' => $result->toArray()]);
+
+        //DB::enableQueryLog();
+        $result = Instructor::with('fichas')->where('id_instructor',3)
+            ->get();
+        dump(['q5d' => $result->toArray()]);
+        //dd(DB::getQueryLog());
+
+
+        $result = Ficha::with('aprendices')->whereIn('id_ficha', [1, 3])->get();
+        dump(['q6' => $result->toArray()]);
+
+
+
+
+        // dd(DB::getQueryLog());
+
+
+        $resultados = Jornada::with('fichas.aprendices', 'fichas.programa', 'fichas.instructores')
+            ->select('nom_jornada', 'id as jornada_id')
+            ->find($jornadaId);
+        //dd(DB::getQueryLog());
+
+        $resultados  = Jornada::with('fichas.aprendices', 'fichas.programa', 'fichas.instructores')
+            ->find($jornadaId);
+
+       // DB::enableQueryLog();
+        $resultados = Jornada::with('fichas.aprendices', 'fichas.programa', 'fichas.instructores')
+            ->where('id', $jornadaId)
+            ->first();
+       // dd(DB::getQueryLog());
+
+        dd('test', $resultados->toArray(),);
+    }
+}
---
+++ resources/views/instructor/index.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.admin.app')
+
+@section('content')
+
+
+    @if (Session::has('create'))
+        <div style="padding: 20px; background-color: #00a65a; color: #ffffff;">
+            {{ Session::get('create') }}
+        </div>
+    @endif
+
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 mx-auto">
+
+
+
+
+
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead class="table-dark">
+                        <tr>
+                            <th>No Documento</th>
+                            <th>Nombres</th>
+                            <th>Apellidos</th>
+                            <th>genero</th>
+                            <th>Tipo de Doc</th>
+                            <th>F. Creación</th>
+                            <th>Foto</th>
+                            <th>Opciones de edición</th>
+                        </tr>
+                        </thead>
+                        <tbody class="table-light">
+                        @foreach($instructor as $instructor)
+<?php
+ $foto = ($instructor->foto??'user.jpg');
+?>
+
+                            <tr>
+                                <td>{{ $instructor->id_instructor }}</td>
+                                <td>{{ $instructor->nom1.' '.$instructor->nom2 }}</td>
+                                <td>{{ $instructor->ape1.' '.$instructor->ape2 }}</td>
+                                <td>{{ $gen[ $instructor->genero ] }}</td>
+                                <td>{{ $doc[ $instructor->tipoDoc ] }}</td>
+                                <td>{{ $instructor->created_at }}</td>
+                                <td>  <img width="80" height="100" class="img-profile ml-3 rounded-circle" src="img/user/{{   $foto }}" alt=""> </td>
+                                <td>
+                                    <form action="{{ route('instructor.destroy',$instructor->id_instructor) }}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <a href="{{ route('instructor.edit',$instructor->id_instructor) }}" class="btn btn-sm btn-warning" title="Editar"><i class="fas fa-eye-dropper"></i></a>
+                                        <button type="submit" class="btn btn-sm btn-danger" title="Eliminar"><i class="fas fa-trash-alt"></i></button>
+                                        <a href="{{ route('instructor.show',$instructor->id_instructor) }}" class="btn btn-sm btn-info" title="Detalle" ><i class="far fa-eye"></i></a>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
